d01ec504b1c4940ccf8acbcff3be6982ee8d8dfb
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-08-08 10:43:28 +0800

    fix

---
+++ cumprod_test.py
@@ -0,0 +1,18 @@
+import tvm
+
+def backward_compute_cumprod()
+    m = tvm.var("m")
+    n = tvm.var("n")
+    X = tvm.compute((m, n), lambda i, j: 1 if i < j else 0)
+    s = tvm.create_schedule(X.op)
+    return s, X
+
+
+s, ret = backward_compute_cumprod()
+f = tvm.build(s, [ret])
+n = 2
+m = 3
+ctx = tvm.cpu()
+a = tvm.nd.array(_np.zeros((m, n), dtype=ret.dtype), ctx)
+f(a)
+print(a)

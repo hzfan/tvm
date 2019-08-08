46a278d5fe3d334130d04af1ff507ac2d5083718
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-08-08 14:07:01 +0800

    fix

--- cumprod_test.py
+++ cumprod_test.py
@@ -29,7 +29,7 @@ def compute_backward_cumprod(dtype, ndim, axis):
                                                                         * X[swapaxis(idx[:-1], 0, axis)])))
     s_scan = tvm.scan(s_init, s_update, s_state)   
     A = tvm.compute(sshape, lambda *idx: s_scan[idx] * out_grad[swapaxis(idx[:-1], 0, axis)])
-    k = tvm.reduce_axis((sshape[0], 0, 0), name="k")
+    k = tvm.reduce_axis((0, sshape[0]), name="k")
     ret = tvm.compute(ishape,
                       lambda* idx: tvm.sum(A[(k,) + idx[:axis] + idx[axis + 1:] + (idx[axis],)],
                                            axis=k), name="ret")

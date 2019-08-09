65dd2e408b16f8fde3b2d461be56463766ed7c32
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-08-09 13:52:42 +0800

    fix

--- cumprod_test.py
+++ cumprod_test.py
@@ -9,8 +9,8 @@ def compute_backward_cumprod(dtype, ndim, axis):
             ret[axis1], ret[axis2] = ret[axis2], ret[axis1]
         return ret if isinstance(idx, list) else tuple(ret)
 
-    ishape = [tvm.var() for _ in range(ndim)]
-    sshape = swapaxis(ishape, 0, axis) + [ishape[axis]]
+    ishape = [tvm.var("i" + i) for i in range(ndim)]
+    sshape = swapaxis(ishape, 0, axis) + [tvm.var("new")]
     X = tvm.placeholder(ishape, dtype=dtype)  # input data
     out_grad = tvm.placeholder(ishape, dtype=dtype)  # output grad
     s_state = tvm.placeholder(sshape, dtype=dtype)

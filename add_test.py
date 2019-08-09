a9d8e7cad3fccbfd15000492898bce32d9aa32cf
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-08-09 16:11:30 +0800

    fix

--- add_test.py
+++ add_test.py
@@ -4,16 +4,16 @@ import numpy as _np
 def compute_add():
     n = tvm.var('n')
     m = tvm.var('m')
-    # X = tvm.placeholder((n, m), dtype='float32', name='X')
-    Y = tvm.placeholder((n, m), dtype='float32', name='Y')
-    Y = tvm.compute((n, m), lambda i, j: Y[i, j] + 1)
+    X = tvm.placeholder((n, m), dtype='float32', name='X')
+    # Y = tvm.placeholder((n, m), dtype='float32', name='Y')
+    Y = tvm.compute((n, m), lambda i, j: X[i, j] + 1)
     s = tvm.create_schedule(Y.op)
-    return s, Y
+    return s, X, Y
 
 
-s, Y = compute_add()
-f = tvm.build(s, [Y])
+s, X, Y = compute_add()
+f = tvm.build(s, [X, Y])
 data_np = _np.array([[1, 2, 3], [4, 5, 6]], dtype='float32')
 data = tvm.nd.array(data_np)
-f(data)
+f(data, data)
 print(data)

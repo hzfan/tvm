35a26ebb4745713c9208529c6b060dbfca7e8f99
Fan <fanhaozh@38f9d3e5ca30.ant.amazon.com>
2019-09-18 21:57:04 +0800

    fix

--- bin_benchmark.py
+++ bin_benchmark.py
@@ -64,7 +64,7 @@ print(tvm.lower(s, [A, B, C], simple_mode=True))
 dsize = 4 
 # ishape_num = [128, 64, 1024]
 # ishape_num = [128 * 256, 256, 1]
-ishape_num = [128 * 64 * 1024]
+ishape_num = [256 * 256 * 256]
 func = tvm.build(s, [A, B, C], target=target, name="add")
 a_np = _np.array(_np.random.uniform(-2.0, 2.0, size=ishape_num), dtype=dtype)
 b_np = _np.array(_np.random.uniform(-2.0, 2.0, size=ishape_num), dtype=dtype)
